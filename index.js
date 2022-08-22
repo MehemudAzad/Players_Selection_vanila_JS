8235d0ed3abd7770228fde10fdd80c396b9064a5
Mr. X <mr.x@Mrs-MacBook-Air.local>
2022-08-22 12:35:47 +0600

    added4

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const selectedSectionEl = document.getElementById('selected-section')
+const selectedSectionEl = document.getElementById('selected-section'); 
+const managerEl = document.getElementById('manager');
+const coachEl = document.getElementById('coach');
+const totalAmountEl = document.getElementById('total');
+const perPlayerPriceEl = document.getElementById('per-player-price')
 var arrPlayers = [];
 function selectPlayer(element){
    
@@ -7,5 +11,13 @@ function selectPlayer(element){
     arrPlayers.push('${playerName}');
    const h3El = document.createElement('h3');
    h3El.innerHTML = `<h5><span>${arrPlayers.length}.</span> ${playerName}</h5>`
-   selectedSectionEl.appendChild(h3El)
+   selectedSectionEl.appendChild(h3El);
+  
+   //disabled button
+   element.disabled = true;
+   //
+}
+
+function calculatePlayerExpanses(){
+
 }
